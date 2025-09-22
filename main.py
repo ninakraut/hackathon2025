3ce87e93d7956e631c85508eabfd4423ab184820
NietroMiner00 <daniel.gerdes.hagen@gmail.com>
2025-09-22 20:26:08 +0000

    Add mapping from criteria to features

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask.templating import render_template
 import json
 
@@ -10,6 +11,33 @@ def main():
 
     return render_template("index.html", data=criteria)
 
-@app.route("/submit")
-def animation():
-    return render_template("animation.html")
+def map_criteria_to_features(criteria_list, mapping):
+    all_features = []
+
+    for criterion in criteria_list:
+        group = criterion["group"]
+        name = criterion["criterion"]
+        if group in mapping and name in mapping[group]:
+            features = mapping[group][name]
+            all_features.extend(features)
+    
+    # Entfernen von Duplikaten und Sortieren der Merkmale
+    unique_features = sorted(list(set(all_features)))
+    
+    return unique_features
+
+@app.route("/submit", methods=["get", "post"])
+def submit():
+    if request.method == "GET":
+        return render_template("animation.html")
+    else:
+        data = request.get_json()
+        if not data or "criteria" not in data:
+            return {"error": "Invalid request data"}, 400
+        
+        criteria_list = data.get("criteria", [])
+        mapping_data = json.load(open("mapping.json"))
+
+        mapped_features = map_criteria_to_features(criteria_list, mapping_data)
+
+        return mapped_features
