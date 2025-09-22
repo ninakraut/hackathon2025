65ec6c7555353f1d588a78d64c2d2be4f940477c
NietroMiner00 <daniel.gerdes.hagen@gmail.com>
2025-09-22 19:53:13 +0000

    Read criteria from file

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
 from flask import Flask
 from flask.templating import render_template
+import json
 
 app = Flask(__name__)
 
-critiria = {
-  "Gestaltung": [
-    "Level of Detail (LOD)",
-    "Level of Information Need (LOIN)",
-    "Visuelle Qualität"
-  ],
-  "Nachhaltigkeit": [
-    "Energieeffizienz",
-    "Cradle-to-Cradle-Prinzip",
-    "Lebenszyklusanalyse (LCA)"
-  ],
-  "Funktionalität": [
-    "Benutzerfreundlichkeit",
-    "Kompatibilität",
-    "Sicherheit"
-  ]
-}
-
 @app.route("/")
 def main():
-    return render_template("index.html", data=critiria)
+    criteria = json.load(open("criteria.json"))
+
+    return render_template("index.html", data=criteria)
 
 @app.route("/submit")
 def animation():
