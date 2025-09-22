bb45ed8e72becde0a224284f19b4caf83e106ef7
NietroMiner00 <daniel.gerdes.hagen@gmail.com>
2025-09-22 19:47:56 +0000

    Add dynamic criteria

--- main.py
+++ main.py
@@ -3,9 +3,27 @@ from flask.templating import render_template
 
 app = Flask(__name__)
 
+critiria = {
+  "Gestaltung": [
+    "Level of Detail (LOD)",
+    "Level of Information Need (LOIN)",
+    "Visuelle Qualität"
+  ],
+  "Nachhaltigkeit": [
+    "Energieeffizienz",
+    "Cradle-to-Cradle-Prinzip",
+    "Lebenszyklusanalyse (LCA)"
+  ],
+  "Funktionalität": [
+    "Benutzerfreundlichkeit",
+    "Kompatibilität",
+    "Sicherheit"
+  ]
+}
+
 @app.route("/")
 def main():
-    return render_template("index.html")
+    return render_template("index.html", data=critiria)
 
 @app.route("/submit")
 def animation():
